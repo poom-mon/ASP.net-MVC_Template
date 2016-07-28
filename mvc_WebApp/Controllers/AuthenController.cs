b9a63908f4a438d08f59240def6b1c84f2c6e6fb
unknown <Sittiporn@PROGRAMMER126.silkspan.local>
2016-07-28 17:58:23 +0700

    update code

---
+++ mvc_WebApp/Controllers/AuthenController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_WebApp.Controllers
+{
+    public class AuthenController : Controller
+    {
+        //
+        // GET: /Authen/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult chkAuthen(ModelAuthen  data)
+        {
+            return Json(
+                     new ModelAuthen { username = data.username, password = data.password }
+                , JsonRequestBehavior.AllowGet);
+        }
+
+
+    }
+
+    public class ModelAuthen {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+
+
+}
